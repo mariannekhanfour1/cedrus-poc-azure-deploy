3c84cbd48764bf0a1ec19052a618a3694214e903
Saeid Eid <saeid@cedrusco.com>
2021-04-04 00:05:10 +0300

    fix rest api

--- src/main/java/com/tutorial/spring/controller/HelloRestController.java
+++ src/main/java/com/tutorial/spring/controller/HelloRestController.java
@@ -4,6 +4,7 @@ import com.tutorial.spring.entities.Greeting;
 import com.tutorial.spring.entities.Person;
 import com.tutorial.spring.services.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRestController {
 
+    @CrossOrigin
     @GetMapping("/rest")
     public Greeting greet(
             @RequestParam(defaultValue = "world", required = false)String name){
         return new Greeting("Hello, "+name+"!");
     }
-    @PostMapping(path = "/foodStamp", consumes = "application/json", produces = "application/json")
+
+    @CrossOrigin
+    @PostMapping(path = "/eligibility", consumes = "application/json", produces = "application/json")
     public boolean shouldGetFoodStamp(@RequestBody Person person) {
         RestClient.checkRule();
-        System.out.print(person.getAge() + person.getMaritalStatus() + person.getInsurance() + person.getIncome());
+        System.out.print(person.getAge() + person.getIncome());
         if(person.getIncome() >= 1000) {
             return false;
         } else {
--- src/main/java/com/tutorial/spring/entities/Person.java
+++ src/main/java/com/tutorial/spring/entities/Person.java
@@ -1,26 +1,8 @@
 package com.tutorial.spring.entities;
 
 public class Person {
-    private boolean insurance;
-    private String maritalStatus;
     private int age;
     private int income;
-
-    public boolean getInsurance() {
-        return insurance;
-    }
-
-    public void setInsurance(Boolean insurance) {
-        this.insurance = insurance;
-    }
-
-    public String getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    public void setMaritalStatus(String maritalStatus) {
-        this.maritalStatus = maritalStatus;
-    }
 
     public int getAge() {
         return age;
